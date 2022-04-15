3fa6d36fa2c7efca7dfa96810a23fdb144b9e553
brockpaperscissors <michaeljbrock91@gmail.com>
2022-04-14 19:57:17 -0700

    Added unique session ID generator

--- src/components/NewSession/NewSession.js
+++ src/components/NewSession/NewSession.js
@@ -1,6 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 export default function NewSession() {
-	let sessionId = '';
+	const navigate = useNavigate();
+	const axios = require('axios').default;
+	let sessionId = 0;
+	const [session, setSession] = useState({
+		id: 0,
+		sessionName: '',
+		numQuestions: 0,
+		sessionQuestions: [''],
+		sessionResponses: [''],
+	});
+
+	function generateSessionID() {
+		return (sessionId = Math.floor(Math.random() * 100000000 + 10000000));
+	}
+
+	function handleClick() {
+		console.log(session);
+		generateSessionID();
+		console.log(sessionId);
+		axios
+			.get('https://badinterviewapi.herokuapp.com/questions', session)
+			.then((res) => {
+				console.log(res);
+				if (res.status === 200) {
+					navigate(`/sessions/${sessionId}`);
+				}
+			});
+	}
+
 	return (
 		<div>
 			<div>
@@ -8,7 +38,7 @@ export default function NewSession() {
 				<p>To get started..</p>
 			</div>
 			<Link to={`/sessions/${sessionId}`}>
-				<button>NEW SESSION</button>
+				<button onClick={handleClick}>NEW SESSION</button>
 			</Link>
 		</div>
 	);
