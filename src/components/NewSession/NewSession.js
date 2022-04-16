d4d2bbd2f842cae34214f9c09b0a35cca0bbda1e
brockpaperscissors <michaeljbrock91@gmail.com>
2022-04-15 19:32:27 -0700

    beginning to map random questions into the user sessions

--- src/components/NewSession/NewSession.js
+++ src/components/NewSession/NewSession.js
@@ -1,44 +1,14 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NewSession() {
-	const navigate = useNavigate();
-	const axios = require('axios').default;
-	let sessionId = 0;
-	const [session, setSession] = useState({
-		id: 0,
-		sessionName: '',
-		numQuestions: 0,
-		sessionQuestions: [''],
-		sessionResponses: [''],
-	});
-
-	function generateSessionID() {
-		return (sessionId = Math.floor(Math.random() * 100000000 + 10000000));
-	}
-
-	function handleClick() {
-		console.log(session);
-		generateSessionID();
-		console.log(sessionId);
-		axios
-			.get('https://badinterviewapi.herokuapp.com/questions', session)
-			.then((res) => {
-				console.log(res);
-				if (res.status === 200) {
-					navigate(`/sessions/${sessionId}`);
-				}
-			});
-	}
-
 	return (
 		<div>
 			<div>
 				<h2>Get Started:</h2>
 				<p>To get started..</p>
 			</div>
-			<Link to={`/sessions/${sessionId}`}>
-				<button onClick={handleClick}>NEW SESSION</button>
+			<Link to={`/sessions/sessionsetup`}>
+				<button>NEW SESSION</button>
 			</Link>
 		</div>
 	);
--- src/components/QuestionCard/QuestionCard.js
+++ src/components/QuestionCard/QuestionCard.js
@@ -1,8 +1,52 @@
+import { useContext, useEffect } from 'react';
+import { SessionContext } from '../../App';
 export default function QuestionCard() {
+	let addQuestion = [];
+	let randomQuestions = [];
+
+	const { session, questions, setSession } = useContext(SessionContext);
+	// console.log(session);
+	// console.log(session.sessionQuestions);
+	// console.log(questions);
+
+	useEffect(() => {
+		if (questions) {
+			addQuestion = [...session.sessionQuestions, ...questions];
+
+			while (randomQuestions.length < session.numQuestions) {
+				if (
+					addQuestion[Math.floor(Math.random() * session.numQuestions)] !== ['']
+				) {
+					randomQuestions.push(
+						addQuestion[Math.floor(Math.random() * session.numQuestions)]
+					);
+					console.log(
+						addQuestion[Math.floor(Math.random() * session.numQuestions)]
+					);
+				}
+				console.log(randomQuestions);
+			}
+			setSession({ ...session, sessionQuestions: randomQuestions });
+		}
+	}, [questions]);
+
 	return (
-		<div>
-			<h2>Question: What is your name?</h2>
-			<textarea row='3' col='25' />
-		</div>
+		<>
+			<div>
+				{session &&
+					session.sessionQuestions.map((question, index) => (
+						<div key={question._id}>
+							<h1>Question: {index + 1}</h1>
+							<h2>Subject: {question.subject}</h2>
+							<h3> {question.body}</h3>
+						</div>
+					))}
+				<textarea row='3' col='25' />
+			</div>
+		</>
 	);
 }
+
+// 	let addQuestion = [
+// 		res.data[Math.floor(Math.random() * session.numQuestions)],
+// 	];
