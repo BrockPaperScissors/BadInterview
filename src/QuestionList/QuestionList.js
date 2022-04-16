66d0a979358f4a40a1d2edfd54e968e89f8b9a67
brockpaperscissors <michaeljbrock91@gmail.com>
2022-04-15 19:33:37 -0700

    got rid of unused files, and created Question list to display all questions in the database

---
+++ src/QuestionList/QuestionList.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import SessionSetup from '../components/SessionSetup/SessionSetup';
+
+export default function QuestionList() {
+	return <h1>hi</h1>;
+}
