329cc1cbf9c8202b9345abb3d7437308ae45e77e
brockpaperscissors <michaeljbrock91@gmail.com>
2022-04-17 00:21:09 -0700

    SessionReview component updated, maps over questions and answers of users session

--- src/components/SessionReview/SessionReview.js
+++ src/components/SessionReview/SessionReview.js
@@ -1,3 +1,37 @@
+import axios from 'axios';
+import { useContext, useEffect } from 'react';
+import { SessionContext } from '../../App';
+
 export default function SessionReview() {
-	return <div>Hello from session review!</div>;
+	const url = 'url';
+	const { session } = useContext(SessionContext);
+	useEffect(() => {
+		console.log(session.sessionId);
+		console.log(session);
+		console.log('post sent');
+		// axios
+		// 	.post('https://badinterviewapi.herokuapp.com/sessions', session)
+		// 	.then((res) => {
+		// 		console.log(res);
+		// 	})
+		// 	.catch((error) => {
+		// 		console.log(error);
+		// 	});
+	}, [session]);
+	return (
+		<>
+			<div>
+				{session &&
+					session.sessionQuestions.map((sessionQuestions, index) => (
+						<div key={index}>
+							<div>
+								<h1>Question: {index + 1}</h1>
+								<h2>Subject: {sessionQuestions.subject}</h2>
+								<p>Response: {session.sessionResponses[index]}</p>
+							</div>
+						</div>
+					))}
+			</div>
+		</>
+	);
 }
