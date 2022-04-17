79758e63744f4111b809026a962b29d63d4e1364
brockpaperscissors <michaeljbrock91@gmail.com>
2022-04-17 00:20:23 -0700

    QuestionCard component functional

--- src/components/QuestionCard/QuestionCard.js
+++ src/components/QuestionCard/QuestionCard.js
@@ -1,52 +1,82 @@
-import { useContext, useEffect } from 'react';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { SessionContext } from '../../App';
 export default function QuestionCard() {
 	let addQuestion = [];
 	let randomQuestions = [];
+	let responseArray = [];
+	let navigate = useNavigate();
 
 	const { session, questions, setSession } = useContext(SessionContext);
-	// console.log(session);
-	// console.log(session.sessionQuestions);
-	// console.log(questions);
 
 	useEffect(() => {
 		if (questions) {
 			addQuestion = [...session.sessionQuestions, ...questions];
 
 			while (randomQuestions.length < session.numQuestions) {
-				if (
-					addQuestion[Math.floor(Math.random() * session.numQuestions)] !== ['']
-				) {
-					randomQuestions.push(
-						addQuestion[Math.floor(Math.random() * session.numQuestions)]
-					);
-					console.log(
-						addQuestion[Math.floor(Math.random() * session.numQuestions)]
-					);
-				}
+				randomQuestions.push(
+					addQuestion[Math.floor(Math.random() * session.numQuestions)]
+				);
+				console.log(
+					addQuestion[Math.floor(Math.random() * session.numQuestions)]
+				);
+
 				console.log(randomQuestions);
 			}
 			setSession({ ...session, sessionQuestions: randomQuestions });
 		}
+		console.log(session.sessionResponses);
 	}, [questions]);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		console.log(session.sessionQuestions.length);
+		console.log(responseArray.length);
+	}
+	function handleChange(e) {
+		responseArray[e.target.id] = e.target.value;
+
+		console.log(responseArray);
+	}
+
+	function handleFinish() {
+		setSession({ ...session, sessionResponses: responseArray });
+	}
 
 	return (
 		<>
 			<div>
 				{session &&
 					session.sessionQuestions.map((question, index) => (
-						<div key={question._id}>
-							<h1>Question: {index + 1}</h1>
-							<h2>Subject: {question.subject}</h2>
-							<h3> {question.body}</h3>
+						<div key={index}>
+							<div>
+								<h1>Question: {index + 1}</h1>
+								<h2>Subject: {question.subject}</h2>
+								<p> {question.body}</p>
+							</div>
+							<form action='' onSubmit={handleSubmit}>
+								<div>
+									<textarea
+										row='3'
+										col='25'
+										placeholder='Type your answer...'
+										onChange={handleChange}
+										id={index}
+										required
+									/>
+									<button type='submit'>Submit</button>
+								</div>
+							</form>
 						</div>
 					))}
-				<textarea row='3' col='25' />
+				{/* {responseArray.length === session.sessionQuestions.length && ( */}
+				<div>
+					<button onClick={handleFinish}>FINISH</button>
+				</div>
+				{/* )} */}
 			</div>
 		</>
 	);
 }
-
-// 	let addQuestion = [
-// 		res.data[Math.floor(Math.random() * session.numQuestions)],
-// 	];
