8f0878d83211177249d7f6f861abf89fb063bafd
brockpaperscissors <michaeljbrock91@gmail.com>
2022-04-17 18:27:34 -0700

    Review works, maps over user input and questions

--- src/components/SessionReview/SessionReview.js
+++ src/components/SessionReview/SessionReview.js
@@ -1,33 +1,40 @@
 import axios from 'axios';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { SessionContext } from '../../App';
 
 export default function SessionReview() {
-	const url = 'url';
 	const { session } = useContext(SessionContext);
+
 	useEffect(() => {
 		console.log(session.sessionId);
 		console.log(session);
 		console.log('post sent');
-		// axios
-		// 	.post('https://badinterviewapi.herokuapp.com/sessions', session)
-		// 	.then((res) => {
-		// 		console.log(res);
-		// 	})
-		// 	.catch((error) => {
-		// 		console.log(error);
-		// 	});
-	}, [session]);
+
+		axios
+			.post('https://badinterviewapi.herokuapp.com/sessions', session)
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+
 	return (
 		<>
 			<div>
 				{session &&
 					session.sessionQuestions.map((sessionQuestions, index) => (
 						<div key={index}>
-							<div>
-								<h1>Question: {index + 1}</h1>
-								<h2>Subject: {sessionQuestions.subject}</h2>
-								<p>Response: {session.sessionResponses[index]}</p>
+							<div className='review-container'>
+								<h1 className='review-number'>Question: {index + 1}</h1>
+								<h2 className='review-subject'>
+									Subject: {sessionQuestions.subject}
+								</h2>
+								<h3 className='review-question'>{sessionQuestions.body}</h3>
+								<p className='review-response'>
+									Response: {session.sessionResponses[index]}
+								</p>
 							</div>
 						</div>
 					))}
