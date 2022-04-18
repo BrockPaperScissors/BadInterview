c23463d0c51b1a990aff32bdfabb6086de63c0e5
brockpaperscissors <michaeljbrock91@gmail.com>
2022-04-17 18:26:10 -0700

    Added CSS classes

--- src/components/NewSession/NewSession.js
+++ src/components/NewSession/NewSession.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 export default function NewSession() {
 	return (
-		<div>
-			<div>
-				<h2>Get Started:</h2>
+		<div className='newsession'>
+			<div className='container'>
+				<h2 className='get-started'>Get Started:</h2>
 				<p>To get started..</p>
 			</div>
 			<Link to={`/sessions/sessionsetup`}>
